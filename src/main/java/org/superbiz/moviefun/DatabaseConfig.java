anusha-vataparambu/apps-movie-fun-code

Implement two data sources

src/main/java/org/superbiz/moviefun/DatabaseConfig.java
@@ -1,0 +1,25 @@
+package org.superbiz.moviefun;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    DatabaseServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapSerivces) {
+        return new DatabaseServiceCredentials(vcapSerivces);
+    }
+
+    @Bean
+    HibernateJpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+        jpaVendorAdapter.setGenerateDdl(true);
+        return jpaVendorAdapter;
+    }
+}

yfujiu/movie-fun

src/main/java/org/superbiz/moviefun/DatabaseConfig.java
@@ -1,0 +1,25 @@
+package org.superbiz.moviefun;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    DatabaseServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapSerivces) {
+        return new DatabaseServiceCredentials(vcapSerivces);
+    }
+
+    @Bean
+    HibernateJpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+        jpaVendorAdapter.setGenerateDdl(true);
+        return jpaVendorAdapter;
+    }
+}